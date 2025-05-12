acf376b1ddf5e4ccc4f68a2166583c43de561a35
Maria Luiza <marialuiza2842@gmail.com>
2025-05-12 18:14:15 -0300

    Delete pysta directory

--- pysta/testefor/for.py
+++
@@ -1,4 +0,0 @@
-frutas = ["maçã", "banana", "laranja"]
-for fruta in frutas:
-    print(fruta)
-# O loop for percorre cada elemento da lista e imprime o nome da fruta
--- pysta/testeif/testefb.py
+++
@@ -1,9 +0,0 @@
-idade = int(input("Digite sua idade: "))
-
-if idade < 18:
-    print("Menor de idade")
-elif idade >= 18 and idade < 60:
-    print("Adulto")
-else:
-    print("erro")
-# Teste de idade
